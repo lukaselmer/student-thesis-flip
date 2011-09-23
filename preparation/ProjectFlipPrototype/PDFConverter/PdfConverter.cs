fa89decd68fbd393f959547816998e19a2655ed6
Lukas Elmer <lukas.elmer@gmail.com>
2011-09-23 12:31:03 +0000

    refs #449 PDFConverter

---
+++ preparation/ProjectFlipPrototype/PDFConverter/PdfConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PdfConverter
+{
+    internal class PdfConverter
+    {
+        public void Convert(string from, string to)
+        {
+            var acrobatLocations = new string[] { @"C:\Program Files (x86)\Adobe\Reader 10.0\Reader\" };
+            var command = "AcroRd32.exe /N /T " + from + " \"Microsoft XPS Document Writer\" /t " + to;
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ preparation/ProjectFlipPrototype/PDFConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PdfConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            if (!CheckForValidArgs(args))
+            {
+                PrintUsage();
+                return;
+            }
+            new PdfConverter().Convert(args[0], args[1]);
+        }
+
+        private static bool CheckForValidArgs(string[] args)
+        {
+            return args.Length == 2;
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Converts a PDF to a XPS file.\n\n" +
+                              "PDFConverter source destination");
+        }
+
+    }
+}
