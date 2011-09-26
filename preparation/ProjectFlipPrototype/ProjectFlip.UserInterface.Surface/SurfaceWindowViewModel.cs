c6533dfd8282a2b5407957b0c14c4bbfab0632af
Christina Heidt <christina.heidt@elmer.ch>
2011-09-26 07:37:08 +0000

    refs #482

---
+++ preparation/ProjectFlipPrototype/ProjectFlip.UserInterface.Surface/SurfaceWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using ProjectFlip.Services.Interfaces;
+
+namespace ProjectFlip.UserInterface.Surface
+{
+    class SurfaceWindowViewModel : INotifyPropertyChanged
+    {
+        
+        public SurfaceWindowViewModel(IProjectNotesService projectNotesService)
+        {
+            ProjectNotes = projectNotesService.ProjectNotes;
+        }
+
+        private void Notify(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public List<IProjectNote> ProjectNotes { get; private set; } 
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
