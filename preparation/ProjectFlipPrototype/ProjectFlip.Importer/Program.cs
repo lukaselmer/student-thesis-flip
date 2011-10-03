5262b5276a941dab4092f8bf1dd1f5fe5c04e7a3
Lukas Elmer <lukas.elmer@gmail.com>
2011-10-03 11:10:14 +0000

    refs #454

---
+++ preparation/ProjectFlipPrototype/ProjectFlip.Importer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjectFlip.Services.Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var importer = new ProjectNotesLoader();
+            var list = importer.Import();
+
+            list.ForEach(line => line.ForEach(Console.WriteLine));
+
+            Console.ReadKey();
+        }
+    }
+}
