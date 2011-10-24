acd35693d3c347936f571d3af0e3dd50d0e2cfc2
Christina Heidt <christina.heidt@elmer.ch>
2011-10-24 13:29:19 +0000

    refs #573

---
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface.Test/ProjectNotesServiceMock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectFlip.Services;
+using ProjectFlip.Services.Interfaces;
+
+namespace ProjectFlip.UserInterface.Surface.Test
+{
+    class ProjectNotesServiceMock : IProjectNotesService
+    {
+
+        private List<IProjectNote> _projectNotes;
+
+        public ProjectNotesServiceMock(int count)
+        {
+            _projectNotes = new List<IProjectNote>();
+            for (int i = 0; i < count; i++)
+            {
+                var pn = new ProjectNote();
+                _projectNotes.Add(pn);
+            }
+        }
+
+        public List<IProjectNote> ProjectNotes
+        {
+            get { return _projectNotes; }
+        }
+    }
+}
---
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/OverviewWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using Microsoft.Surface.Presentation.Controls;
+using ProjectFlip.Services.Interfaces;
+
+namespace ProjectFlip.UserInterface.Surface
+{
+    public class OverviewWindowViewModel : INotifyPropertyChanged
+    {
+
+        public OverviewWindowViewModel(IProjectNotesService projectNotesService)
+        {
+            ProjectNotes = projectNotesService.ProjectNotes;
+        }
+
+        private void Notify(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public List<IProjectNote> ProjectNotes { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
