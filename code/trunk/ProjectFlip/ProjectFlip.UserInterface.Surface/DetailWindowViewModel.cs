3885c51cfe8d843b7999bf0bb5eba751a8c6891f
Delia Treichler <delia.teichler@elmer.ch>
2011-10-24 13:54:56 +0000

    refs #575

---
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/DetailWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectFlip.UserInterface.Surface
+{
+    public class DetailWindowViewModel : ViewModelBase
+    {
+
+    }
+}
