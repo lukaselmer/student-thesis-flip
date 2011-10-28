c79455b62cc9165d15d09648865cc20808bc8b89
Christina Heidt <christina.heidt@elmer.ch>
2011-10-28 08:07:27 +0000

    refs #579

---
+++ code/trunk/ProjectFlip/ProjectFlip.Services.Test/ProjectNotesServiceTest.cs
@@ -0,0 +1,94 @@
+﻿using ProjectFlip.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProjectFlip.Services.Loader.Interfaces;
+using ProjectFlip.Services.Interfaces;
+using System.Collections.Generic;
+
+namespace ProjectFlip.Services.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProjectNotesServiceTest and is intended
+    ///to contain all ProjectNotesServiceTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProjectNotesServiceTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ProjectNotesService Constructor
+        ///</summary>
+        [TestMethod()]
+        public void ProjectNotesServiceConstructorTest()
+        {
+            IProjectNotesLoader projectNotesLoader = null; // TODO: Initialize to an appropriate value
+            ProjectNotesService target = new ProjectNotesService(projectNotesLoader);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for ProjectNotes
+        ///</summary>
+        [TestMethod()]
+        public void ProjectNotesTest()
+        {
+            IProjectNotesLoader projectNotesLoader = null; // TODO: Initialize to an appropriate value
+            ProjectNotesService target = new ProjectNotesService(projectNotesLoader); // TODO: Initialize to an appropriate value
+            List<IProjectNote> actual;
+            actual = target.ProjectNotes;
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
