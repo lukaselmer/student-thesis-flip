8f9ca883d47ea38ed3303de3f54da9b612333b90
Delia Treichler <delia.teichler@elmer.ch>
2011-10-28 15:12:14 +0000

    refs #582

--- code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface.Test/DetailWindowViewModelTest.cs
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface.Test/DetailWindowViewModelTest.cs
@@ -6,18 +6,13 @@ using System.Windows.Documents;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProjectFlip.Services;
 using ProjectFlip.Services.Interfaces;
+using ProjectFlip.UserInterface.Surface;
 
 namespace ProjectFlip.UserInterface.Surface.Test
 {
     [TestClass]
     public class DetailWindowViewModelTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-
-
         [TestMethod]
         public void DetailWindowViewModelConstructorTest()
         {
@@ -26,10 +21,37 @@ namespace ProjectFlip.UserInterface.Surface.Test
             LinkedListNode<IProjectNote> currentNode = new LinkedListNode<IProjectNote>(service.ProjectNotes[2]);
             IDocumentPaginatorSource doc = currentNode.Value.Document;
             DetailWindowViewModel target = new DetailWindowViewModel(service, service.ProjectNotes[2]);
+        }
 
+        /// <summary>
+        ///A test for NavigateToLeft
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("ProjectFlip.UserInterface.Surface.dll")]
+        public void NavigateToLeftTest()
+        {
+            IProjectNotesService service = new ProjectNotesServiceMock(4);
+            IProjectNote currentPN = (service.ProjectNotes[2]);
+            IProjectNote previousPN = (service.ProjectNotes[1]);
+            DetailWindowViewModel_Accessor target = new DetailWindowViewModel_Accessor(service, currentPN);
+            object parameter = new object(); // TODO: Initialize to an appropriate value
+            target.NavigateToLeft(parameter);
+            Assert.AreEqual(previousPN.Document,currentPN.Document);
+            //Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
 
-           
-            //Document
+        /// <summary>
+        ///A test for NavigateToRight
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("ProjectFlip.UserInterface.Surface.dll")]
+        public void NavigateToRightTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            DetailWindowViewModel_Accessor target = new DetailWindowViewModel_Accessor(param0); // TODO: Initialize to an appropriate value
+            object parameter = null; // TODO: Initialize to an appropriate value
+            target.NavigateToRight(parameter);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
         }
     }
 }
