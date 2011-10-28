32e6c81bd6a3e6df486c3892f2fd6d1527ec6fc0
Delia Treichler <delia.teichler@elmer.ch>
2011-10-28 15:14:22 +0000

    refs #582

--- code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface.Test/ProjectNotesServiceMock.cs
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface.Test/ProjectNotesServiceMock.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
 using ProjectFlip.Services;
 using ProjectFlip.Services.Interfaces;
 
@@ -14,7 +17,7 @@ namespace ProjectFlip.UserInterface.Surface.Test
             _projectNotes = new List<IProjectNote>();
             for (int i = 0; i < count; i++)
             {
-                var pn = new ProjectNote();
+                var pn = new ProjectNoteMock();
                 _projectNotes.Add(pn);
             }
         }
@@ -22,6 +25,55 @@ namespace ProjectFlip.UserInterface.Surface.Test
         public List<IProjectNote> ProjectNotes
         {
             get { return _projectNotes; }
+        }
+
+
+    }
+
+    internal class ProjectNoteMock : IProjectNote
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public string Sector { get; set; }
+
+        public string Customer { get; set; }
+
+        public IList<string> Focus { get; set; }
+
+        public IList<string> Services { get; set; }
+
+        public IList<string> Technologies { get; set; }
+
+        public IList<string> Applications { get; set; }
+
+        public IList<string> Tools { get; set; }
+
+        public System.DateTime Published { get; set; }
+
+        public string Filename { get; set; }
+
+        public string FilepathPdf { get; set; }
+
+        public string FilepathXps { get; set; }
+
+        public string FilepathImage { get; set; }
+
+        public string Url { get; set; }
+
+        public IList<string> Line { get; set; }
+
+        public System.Windows.Media.Imaging.BitmapImage Image { get; set; }
+
+        public System.Windows.Documents.IDocumentPaginatorSource Document
+        {
+            get
+            {
+                return new System.Windows.Documents.FixedDocument();
+            }
         }
     }
 }
