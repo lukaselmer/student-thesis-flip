d0ffb127b184b4e0419ffdcd9d4d81a73600522f
Lukas Elmer <lukas.elmer@gmail.com>
2011-10-31 12:48:12 +0000

    refs #576 Übersicht -> Detail

--- code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/DetailWindowViewModel.cs
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/DetailWindowViewModel.cs
@@ -13,9 +13,9 @@ namespace ProjectFlip.UserInterface.Surface
     public class DetailWindowViewModel : ViewModelBase
     {
         public IDocumentPaginatorSource Document { get; private set; }
-        public UserInterface.Command NavigateToLeftCommand { get; private set; }
-        public UserInterface.Command NavigateToRightCommand { get; private set; }
-        public UserInterface.Command CloseWindowCommand { get; private set; }
+        public Command NavigateToLeftCommand { get; private set; }
+        public Command NavigateToRightCommand { get; private set; }
+        public Command CloseWindowCommand { get; private set; }
         private LinkedList<IProjectNote> ProjectNotes { get; set; }
         private LinkedListNode<IProjectNote> _currentNode;
         public event Action CloseWindow;
@@ -25,9 +25,9 @@ namespace ProjectFlip.UserInterface.Surface
             ProjectNotes = new LinkedList<IProjectNote>(projectNotesService.ProjectNotes);
             _currentNode = ProjectNotes.Find(projectNote);
             Document = _currentNode.Value.Document;
-            NavigateToLeftCommand = new UserInterface.Command(NavigateToLeft);
-            NavigateToRightCommand = new UserInterface.Command(NavigateToRight);
-            CloseWindowCommand = new UserInterface.Command((sender) => { if (CloseWindow != null) CloseWindow(); });
+            NavigateToLeftCommand = new Command(NavigateToLeft);
+            NavigateToRightCommand = new Command(NavigateToRight);
+            CloseWindowCommand = new Command((sender) => { if (CloseWindow != null) CloseWindow(); });
         }
 
         private void NavigateToLeft(object parameter)
