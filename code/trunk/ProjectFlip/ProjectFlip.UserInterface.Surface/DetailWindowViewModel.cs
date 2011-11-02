5c063ea7d03e7618f99e4bf6e4e5cdfe053dabc0
Lukas Elmer <lukas.elmer@gmail.com>
2011-11-02 16:57:13 +0000

    refs #593 Refactoring

--- code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/DetailWindowViewModel.cs
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/DetailWindowViewModel.cs
@@ -1,24 +1,17 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Xps.Packaging;
 using ProjectFlip.Services.Interfaces;
 
+#endregion
 
 namespace ProjectFlip.UserInterface.Surface
 {
     public class DetailWindowViewModel : ViewModelBase
     {
-        public IDocumentPaginatorSource Document { get; private set; }
-        public Command NavigateToLeftCommand { get; private set; }
-        public Command NavigateToRightCommand { get; private set; }
-        public Command CloseWindowCommand { get; private set; }
-        private LinkedList<IProjectNote> ProjectNotes { get; set; }
         private LinkedListNode<IProjectNote> _currentNode;
-        public event Action CloseWindow;
 
         public DetailWindowViewModel(IProjectNotesService projectNotesService, IProjectNote projectNote)
         {
@@ -27,8 +20,19 @@ namespace ProjectFlip.UserInterface.Surface
             Document = _currentNode.Value.Document;
             NavigateToLeftCommand = new Command(NavigateToLeft);
             NavigateToRightCommand = new Command(NavigateToRight);
-            CloseWindowCommand = new Command((sender) => { if (CloseWindow != null) CloseWindow(); });
+            CloseWindowCommand = new Command(sender => { if (CloseWindow != null) CloseWindow(); });
         }
+
+        // ReSharper disable MemberCanBePrivate.Global
+        // ReSharper disable UnusedAutoPropertyAccessor.Global
+        public IDocumentPaginatorSource Document { get; private set; }
+        public Command NavigateToLeftCommand { get; private set; }
+        public Command NavigateToRightCommand { get; private set; }
+        public Command CloseWindowCommand { get; private set; }
+        private LinkedList<IProjectNote> ProjectNotes { get; set; }
+        public event Action CloseWindow;
+        // ReSharper restore UnusedAutoPropertyAccessor.Global
+        // ReSharper restore MemberCanBePrivate.Global
 
         private void NavigateToLeft(object parameter)
         {
@@ -48,5 +52,4 @@ namespace ProjectFlip.UserInterface.Surface
             Notify("Document");
         }
     }
-
 }
