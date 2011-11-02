4887244d04d16ece136986a536567dedab5091f6
Lukas Elmer <lukas.elmer@gmail.com>
2011-11-02 16:55:25 +0000

    refs #593 Refactoring

--- code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/Command.cs
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/Command.cs
@@ -1,14 +1,18 @@
+#region
+
 using System;
 using System.Windows.Input;
+
+#endregion
 
 namespace ProjectFlip.UserInterface.Surface
 {
     public class Command : ICommand
     {
-        private readonly Action<object> _execute;
         private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
 
-        public Command(Action<object> execute, Predicate<object> canExecute )
+        public Command(Action<object> execute, Predicate<object> canExecute)
         {
             _execute = execute;
             _canExecute = canExecute;
@@ -19,6 +23,8 @@ namespace ProjectFlip.UserInterface.Surface
             _execute = execute;
         }
 
+        #region ICommand Members
+
         public void Execute(object parameter)
         {
             _execute(parameter);
@@ -26,21 +32,16 @@ namespace ProjectFlip.UserInterface.Surface
 
         public bool CanExecute(object parameter)
         {
-            if (_canExecute == null)
-            {
-                return true;
-            }
-            return _canExecute(parameter);
-        }
-
-        public void RaiseCanExecuteChanged()
-        {
-            if (CanExecuteChanged != null)
-            {
-                CanExecuteChanged(this, EventArgs.Empty);
-            }
+            return _canExecute == null || _canExecute(parameter);
         }
 
         public event EventHandler CanExecuteChanged;
+
+        #endregion
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null) CanExecuteChanged(this, EventArgs.Empty);
+        }
     }
 }
