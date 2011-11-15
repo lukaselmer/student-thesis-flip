2b951a653469c235769c3b9cf9e1ba2300394265
Lukas Elmer <lukas.elmer@gmail.com>
2011-11-15 00:14:27 +0000

    refactoring

--- code/trunk/ProjectFlip/ProjectFlip.Services/Aggregator.cs
+++ code/trunk/ProjectFlip/ProjectFlip.Services/Aggregator.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Odbc;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -12,44 +10,36 @@ namespace ProjectFlip.Services
     public class Aggregator : IAggregator
     {
         private const string Separator = "\t";
-        public string MappingFilePath = @"..\..\..\Resources\mapping.txt";
+        private const string MappingFilePath = @"..\..\..\Resources\mapping.txt";
         Dictionary<string, IMetadata> _mapping = new Dictionary<string, IMetadata>();
-        private readonly string[] Header = new[] { "Kategorie", "Mapping nach", "Mapping von 1", "Mapping von 2", "Mapping von 3", "usw. ..." };
+        private readonly string[] _header = new[] { "Kategorie", "Mapping nach", "Mapping von 1", "Mapping von 2", "Mapping von 3", "usw. ..." };
 
         public void LoadMapping()
         {
-            try
-            {
-                if (!File.Exists(MappingFilePath)) return;
-                _mapping = new Dictionary<string, IMetadata>();
+            if (!File.Exists(MappingFilePath)) return;
+            _mapping = new Dictionary<string, IMetadata>();
 
-                using (var handle = new StreamReader(MappingFilePath))
+            using (var handle = new StreamReader(MappingFilePath))
+            {
+                handle.ReadLine(); // Skip header line
+                while (!handle.EndOfStream)
                 {
-                    handle.ReadLine(); // Skip header line
-                    while (!handle.EndOfStream)
-                    {
-                        var line = handle.ReadLine();
-                        Debug.Assert(line != null, "line != null");
-                        var elements = line.Split(new[] { Separator.ToCharArray()[0] });
-                        if (elements.Length < 2) continue;
-                        var category = elements[0];
-                        var mapTo = elements[1];
-                        var mappingsTo = elements.Skip(2);
-                        mappingsTo.ToList().ForEach(el => _mapping[el] = Metadata.Get(MetadataType.Get(category), mapTo));
-                    }
+                    var line = handle.ReadLine();
+                    Debug.Assert(line != null, "line != null");
+                    var elements = line.Split(new[] { Separator.ToCharArray()[0] });
+                    if (elements.Length < 2) continue;
+                    var category = elements[0];
+                    var mapTo = elements[1];
+                    var mappingsTo = elements.Skip(2);
+                    mappingsTo.ToList().ForEach(el => _mapping[el] = Metadata.Get(MetadataType.Get(category), mapTo));
                 }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
             }
         }
 
         public void SaveMapping()
         {
             var reverseMapping = ReverseMapping();
-            var lines = new List<List<string>> { Header.ToList() };
+            var lines = new List<List<string>> { _header.ToList() };
             reverseMapping.Keys.ToList().ForEach(metadata =>
             {
                 var line = new List<string> { metadata.Type.Name, metadata.Description };
