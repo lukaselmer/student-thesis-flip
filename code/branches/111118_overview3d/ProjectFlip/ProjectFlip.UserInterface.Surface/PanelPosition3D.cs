b1280e4270a2b0c018eceb3987f4356ca7ed401f
Lukas Elmer <lukas.elmer@gmail.com>
2011-11-20 19:16:31 +0000

    3D Overview

--- code/branches/111118_overview3d/ProjectFlip/ProjectFlip.UserInterface.Surface/PanelPosition3D.cs
+++ code/branches/111118_overview3d/ProjectFlip/ProjectFlip.UserInterface.Surface/PanelPosition3D.cs
@@ -8,7 +8,7 @@ namespace ProjectFlip.UserInterface.Surface
 {
     internal class PanelPosition3D
     {
-        private Dictionary<int, int> LineHeights;
+        private int[] LineHeights;
         public Size FinalSize { get; set; }
         public List<FrameworkElement> Children { get; private set; }
         public int CountX { get; private set; }
@@ -16,8 +16,9 @@ namespace ProjectFlip.UserInterface.Surface
         public int ItemWidth { get; private set; }
         public int ItemHeight { get; private set; }
         public int Index;
-        public int XIndex { get { return Index == 0 ? 0 : CountX % Index; } }
-        public int YIndex { get { return Index == 0 ? 0 : CountY / Index; } }
+        private int UnscaledItemHeight;
+        public int XIndex { get { return Index == 0 ? 0 : Index % CountY; } }
+        public int YIndex { get { return Index == 0 ? 0 : Index / CountX ; } }
 
         public PanelPosition3D(Size finalSize, List<FrameworkElement> children)
         {
@@ -25,21 +26,21 @@ namespace ProjectFlip.UserInterface.Surface
             FinalSize = finalSize;
             Children = children;
             var itemWidth = (int)children.Max(el => el.DesiredSize.Width) + 1;
-            var itemHeight = (int)children.Max(el => el.DesiredSize.Height) + 1;
+            UnscaledItemHeight = (int)children.Max(el => el.DesiredSize.Height) + 1;
 
             CountX = (int)(finalSize.Width / itemWidth) - 1;
             CountY = Children.Count / CountX;
             ItemWidth = itemWidth;
-            ItemHeight = itemHeight;
+            ItemHeight = UnscaledItemHeight;
 
-            LineHeights = new Dictionary<int, int>();
-            while (MoveToNext())
-            {
-                if (!LineHeights.ContainsKey(YIndex)) LineHeights[YIndex] = 0;
-                LineHeights[YIndex] = (int)(Math.Max(children[Index].DesiredSize.Height, LineHeights[YIndex]) * CalculateScale(YIndex));
-            }
+            LineHeights = Enumerable.Range(0, CountY).Select(i => UnscaledItemHeight * (int)CalculateScale(i)).ToArray();
+            //while (MoveToNext())
+            //{
+            //if (!LineHeights.ContainsKey(YIndex)) LineHeights[YIndex] = 0;
+            //LineHeights[YIndex] = (int)(Math.Max(children[Index].DesiredSize.Height, LineHeights[YIndex]) * CalculateScale(YIndex));
+            //}
             Console.WriteLine();
-            Console.WriteLine(LineHeights.Count);
+            Console.WriteLine(LineHeights.Length);
             Console.WriteLine(Children.Count);
             Console.WriteLine(CountX);
             Console.WriteLine(CountY);
@@ -50,8 +51,10 @@ namespace ProjectFlip.UserInterface.Surface
 
         public int TotalLineHeight(int line)
         {
+            return LineHeights[line] + (line <= 0 ? 0 : TotalLineHeight(line - 1));
             //LineHeights[line]
-            return LineHeights.ToList().GetRange(0, line + 1).Sum(x => x.Key + x.Value);
+            return LineHeights.ToList().GetRange(0, line + 1).Sum();
+            //return LineHeights.ToList().GetRange(0, line + 1).Sum(x => x.Key + x.Value);
             //var elementsOnCurrentLine = Children.GetRange(line * CountX, 1); // CountY
             //return (int)elementsOnCurrentLine.Max(el => el.DesiredSize.Height) + (line == 0 ? 0 : TotalLineHeight(line - 1));
         }
