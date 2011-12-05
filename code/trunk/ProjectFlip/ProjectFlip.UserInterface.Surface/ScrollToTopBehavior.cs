ec9cffb8ee1aae72244cc2dea32555e53585ed03
Delia Treichler <delia.teichler@elmer.ch>
2011-12-05 17:37:29 +0000

    refs #623

--- code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/ScrollToTopBehavior.cs
+++ code/trunk/ProjectFlip/ProjectFlip.UserInterface.Surface/ScrollToTopBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -84,4 +85,177 @@ namespace ProjectFlip.UserInterface.Surface
             return child;
         }
     }
+
+    /// <summary>
+    /// This class contains a few useful extenders for the ListBox
+    /// </summary>
+    public class ListBoxExtenders : DependencyObject
+    {
+        #region Properties
+
+        public static readonly DependencyProperty AutoScrollToCurrentItemProperty = 
+            DependencyProperty.RegisterAttached("AutoScrollToCurrentItem", 
+            typeof(bool), typeof(ListBoxExtenders), 
+            new UIPropertyMetadata(default(bool), OnAutoScrollToCurrentItemChanged));
+
+        /// <summary>
+        /// Returns the value of the AutoScrollToCurrentItemProperty
+        /// </summary>
+        /// <param name="obj">The dependency-object whichs value should be returned</param>
+        /// <returns>The value of the given property</returns>
+        public static bool GetAutoScrollToCurrentItem(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollToCurrentItemProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the AutoScrollToCurrentItemProperty
+        /// </summary>
+        /// <param name="obj">The dependency-object whichs value should be set</param>
+        /// <param name="value">The value which should be assigned to the AutoScrollToCurrentItemProperty</param>
+        public static void SetAutoScrollToCurrentItem(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollToCurrentItemProperty, value);
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// This method will be called when the AutoScrollToCurrentItem
+        /// property was changed
+        /// </summary>
+        /// <param name="s">The sender (the ListBox)</param>
+        /// <param name="e">Some additional information</param>
+        public static void OnAutoScrollToCurrentItemChanged
+            (DependencyObject s, DependencyPropertyChangedEventArgs e)
+        {
+            var listBox = s as ListBox;
+            if (listBox != null)
+            {
+                var listBoxItems = listBox.Items;
+                if (listBoxItems != null)
+                {
+                    var newValue = (bool)e.NewValue;
+
+                    var autoScrollToCurrentItemWorker = new EventHandler((s1, e2) => OnAutoScrollToCurrentItem(listBox, listBox.Items.CurrentPosition));
+
+                    if (newValue)
+                        listBoxItems.CurrentChanged += autoScrollToCurrentItemWorker;
+                    else
+                        listBoxItems.CurrentChanged -= autoScrollToCurrentItemWorker;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method will be called when the ListBox should
+        /// be scrolled to the given index
+        /// </summary>
+        /// <param name="listBox">The ListBox which should be scrolled</param>
+        /// <param name="index">The index of the item to which it should be scrolled</param>
+        public static void OnAutoScrollToCurrentItem(ListBox listBox, int index)
+        {
+            if (listBox != null && listBox.Items != null && listBox.Items.Count > index && index >= 0)
+                listBox.ScrollIntoView(listBox.Items[index]);
+        }
+
+        #endregion
+    }
+
+
+
+    public class ListBoxBehavior
+    {
+        static Dictionary<ListBox, Capture> Associations =
+            new Dictionary<ListBox, Capture>();
+
+        public static readonly DependencyProperty ScrollOnNewItemProperty =
+         DependencyProperty.RegisterAttached(
+             "ScrollOnNewItem",
+             typeof(bool),
+             typeof(ListBoxBehavior),
+             new UIPropertyMetadata(false, OnScrollOnNewItemChanged));
+
+        public static bool GetScrollOnNewItem(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ScrollOnNewItemProperty);
+        }
+
+        public static void SetScrollOnNewItem(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ScrollOnNewItemProperty, value);
+        }
+
+
+
+        public static void OnScrollOnNewItemChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var listBox = d as ListBox;
+            if (listBox == null) return;
+            bool oldValue = (bool)e.OldValue, newValue = (bool)e.NewValue;
+            if (newValue == oldValue) return;
+            if (newValue)
+            {
+                listBox.Loaded += new RoutedEventHandler(ListBox_Loaded);
+                listBox.Unloaded += new RoutedEventHandler(ListBox_Unloaded);
+            }
+            else
+            {
+                listBox.Loaded -= ListBox_Loaded;
+                listBox.Unloaded -= ListBox_Unloaded;
+                if (Associations.ContainsKey(listBox))
+                    Associations[listBox].Dispose();
+            }
+        }
+
+        static void ListBox_Unloaded(object sender, RoutedEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+            Associations[listBox].Dispose();
+            listBox.Unloaded -= ListBox_Unloaded;
+        }
+
+        static void ListBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+            var incc = listBox.Items as INotifyCollectionChanged;
+            if (incc == null) return;
+            listBox.Loaded -= ListBox_Loaded;
+            Associations[listBox] = new Capture(listBox);
+        }
+
+        class Capture : IDisposable
+        {
+            public ListBox listBox { get; set; }
+            public INotifyCollectionChanged incc { get; set; }
+
+            public Capture(ListBox listBox)
+            {
+                this.listBox = listBox;
+                incc = listBox.ItemsSource as INotifyCollectionChanged;
+                incc.CollectionChanged +=
+                    new NotifyCollectionChangedEventHandler(incc_CollectionChanged);
+            }
+
+            void incc_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    listBox.ScrollIntoView(e.NewItems[0]);
+                    listBox.SelectedItem = e.NewItems[0];
+                }
+            }
+
+            public void Dispose()
+            {
+                incc.CollectionChanged -= incc_CollectionChanged;
+            }
+        }
+    }
+
+
 }
