50280d5f35a03702dbbc18ee95b925e8ca0e690e
Lukas Elmer <lukas.elmer@gmail.com>
2011-12-14 16:55:21 +0000

    refs #638 Code comments

--- code/trunk/ProjectFlip/ProjectFlip.Preparer/ImageExtractorProcessor.cs
+++ code/trunk/ProjectFlip/ProjectFlip.Preparer/ImageExtractorProcessor.cs
@@ -17,16 +17,35 @@ using ProjectFlip.Services.Interfaces;
 
 namespace ProjectFlip.Preparer
 {
+    /// <summary>
+    /// Extracts a image from the XPS document.
+    /// </summary>
+    /// <remarks></remarks>
     internal class ImageExtractorProcessor : IProcessor
     {
         #region Other
 
+        /// <summary>
+        /// Extracts a image from the XPS document.
+        /// </summary>
+        /// <param name="projectNote">The project note.</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Normally, nnly the GUI thread uses the XpsDocument. Unfortunatly Microsoft did
+        /// not publish a version which can be run in a non GUI thread. Therefore, the ApartmentState
+        /// of the thread has to be set to ApartmentState.STA.
+        /// 
+        /// So this should be replaced as soon as there is a better alternative availible.
+        /// </remarks>
         public bool Process(IProjectNote projectNote)
         {
             if (!File.Exists(projectNote.FilepathXps)) return false;
             if (File.Exists(projectNote.FilepathImage)) return false;
 
             var sta = new Thread(() => ExtractImage(projectNote.FilepathXps, projectNote.FilepathImage));
+            // Normally, nnly the GUI thread uses the XpsDocument. Unfortunatly Microsoft did
+            // not publish a version which can be run in a non GUI thread. Therefore, the ApartmentState
+            // of the thread has to be set to ApartmentState.STA.
             sta.SetApartmentState(ApartmentState.STA);
             sta.Start();
             sta.Join();
@@ -34,6 +53,16 @@ namespace ProjectFlip.Preparer
             return true;
         }
 
+        /// <summary>
+        /// Extracts the image from the XPS file and saves it to the image path.
+        /// </summary>
+        /// <param name="xpsPath">The XPS path.</param>
+        /// <param name="imagePath">The image path.</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// There is a known memory leak, <see cref="http://stackoverflow.com/questions/218681/opening-xps-document-in-net-causes-a-memory-leak"/>.
+        /// In the finally block, the workaround is applied.
+        /// </remarks>
         private static bool ExtractImage(string xpsPath, string imagePath)
         {
             try
@@ -63,6 +92,12 @@ namespace ProjectFlip.Preparer
             }
         }
 
+        /// <summary>
+        /// Processes the document and extracts the image.
+        /// </summary>
+        /// <param name="xpsPath">The XPS path.</param>
+        /// <param name="imagePath">The image path.</param>
+        /// <remarks></remarks>
         private static void ProcessDocument(string xpsPath, string imagePath)
         {
             using (var xpsDoc = new XpsDocument(xpsPath, FileAccess.Read))
@@ -78,6 +113,13 @@ namespace ProjectFlip.Preparer
             }
         }
 
+        /// <summary>
+        /// Extracts the full image from the IDocumentPaginatorSource and saves it to
+        /// a temporary path.
+        /// </summary>
+        /// <param name="tempImagePath">The temp image path.</param>
+        /// <param name="docSeq">The doc seq.</param>
+        /// <remarks></remarks>
         private static void XpsToTempImage(string tempImagePath, IDocumentPaginatorSource docSeq)
         {
             using (var docPage = docSeq.DocumentPaginator.GetPage(0))
@@ -96,6 +138,13 @@ namespace ProjectFlip.Preparer
             }
         }
 
+        /// <summary>
+        /// Cropps the temporary image (the full page view), so that the image at the position between
+        /// 340x309 and 583x552 is cropped out. It then saves it to the real image path.
+        /// </summary>
+        /// <param name="imagePath">The image path.</param>
+        /// <param name="tempImagePath">The temp image path.</param>
+        /// <remarks></remarks>
         private static void TempImageToCroppedImage(string imagePath, string tempImagePath)
         {
             using (var v = Image.FromFile(tempImagePath))
